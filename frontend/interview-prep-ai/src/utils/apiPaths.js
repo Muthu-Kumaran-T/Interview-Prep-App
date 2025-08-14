83580e5db22ad326171f00ebfb2a5bd83a3091bd
Muthukumaran1365 <muth22097.cs@rmkec.ac.in>
2025-08-14 21:45:28 +0530

    Updated

--- frontend/interview-prep-ai/src/utils/apiPaths.js
+++ frontend/interview-prep-ai/src/utils/apiPaths.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "https://interview-prep-app-umgq.onrender.com";
+export const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:8000";
 
 export const API_PATHS = {
     AUTH: {
